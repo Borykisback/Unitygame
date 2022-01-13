52d0b266d3e12e97e48cc3372fa631aff76a8d23
Borykisback <bokback@mail.ru>
2022-01-13 23:38:57 +0300

    first

---
+++ Assets/script/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float timeLeft = 2;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
